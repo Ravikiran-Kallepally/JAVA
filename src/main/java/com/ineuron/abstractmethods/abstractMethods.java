0f063ccb13502e241724608e73f5adaad3c1b2d4
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-04-21 06:37:12 -0500

    Revised java practice day 3 methods

---
+++ src/main/java/com/ineuron/abstractmethods/abstractMethods.java
@@ -0,0 +1,27 @@
+package com.ineuron.abstractmethods;
+
+abstract class vehicle{
+    public abstract int getNoOfWheels();
+}
+
+class Bus extends vehicle{
+    public int getNoOfWheels(){
+        return 7;
+    }
+}
+
+class Auto extends vehicle{
+    public int getNoOfWheels(){
+        return 3;
+    }
+}
+
+
+
+public class abstractMethods {
+
+    public static void main(String[] args){
+        System.out.println("Abstract methods practice ");
+
+    }
+}
