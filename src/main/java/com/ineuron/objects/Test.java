15159f210122f0dee5677269080cf524d9af8bc2
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-04-21 07:29:57 -0500

    Revised java practice day 3 access objects

---
+++ src/main/java/com/ineuron/objects/Test.java
@@ -0,0 +1,21 @@
+package com.ineuron.objects;
+
+ class Test {
+    final int i =10;
+     public static void main(String[] args){
+         Test t = new Test();
+         System.out.println(t.i);
+     }
+}
+
+class Testing {
+    int i;
+
+    public static void main(String[] args) {
+        Testing t_t = new Testing();
+        System.out.println(t_t.i);
+
+    }
+}
+
+
