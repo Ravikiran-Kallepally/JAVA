8662e2a406b110bc9651f0bc630cf604584219cf
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-04-26 09:04:33 -0500

    Revised java practice day 8 Packages&Import  practice (corejavapractice/PackagesandImport)& created a separate package corejavamath for this excercise ,only solved 1 problem => problem number 101 : Good Start

---
+++ src/main/java/com/ineuron/corejavamath/Calculator.java
@@ -0,0 +1,13 @@
+package com.ineuron.corejavamath;
+
+public class Calculator {
+
+    public static void logSomething(){
+        System.out.println("I am coming from core java math package");
+    }
+
+    public static void main(String[] args){
+        System.out.println("Separate package for corejava , and I am trying to use these methods in a different package");
+    }
+
+}
---
+++ src/main/java/com/ineuron/corejavapractice/PackagesandImport.java
@@ -0,0 +1,9 @@
+package com.ineuron.corejavapractice;
+import com.ineuron.corejavamath.Calculator;
+
+public class PackagesandImport {
+    public static void main(String[] args){
+        System.out.println("Packages and Import class file from core java folder ");
+        Calculator.logSomething(); //This will log the message to the console
+    }
+}
