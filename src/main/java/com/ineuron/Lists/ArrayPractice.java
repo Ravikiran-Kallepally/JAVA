87e8f1a8d21f46735da51493dd7d54d24383df2c
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-04-29 08:03:50 -0500

    Revised java practice day 11 ArrayList  Questions 126,127  => completed, conceptually

--- src/main/java/com/ineuron/Lists/ArrayPractice.java
+++ src/main/java/com/ineuron/Lists/ArrayPractice.java
@@ -2,6 +2,8 @@ package com.ineuron.Lists;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+
 
 class Student{
     String name ="RK";
@@ -13,11 +15,75 @@ class Student{
     }
 }
 
+class Book{
+    String name;
+    double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Book(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Book1{
+    String name;
+    double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Book1(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+
+
+
+
 public class ArrayPractice {
+
+    public static double calculateAverage(List<Integer> marks){
+        int sum =0;
+        for(int mark:marks){
+            sum+=mark;
+        }
+        //System.out.println("Average marks of this student are : "+(double)sum/marks.size());
+        return (double)sum/marks.size();
+    }
+
+
+
+
+
+
     public static void main(String[] args){
         ArrayList<Integer> myNumbers = new ArrayList<Integer>();
         ArrayList<String> str = new ArrayList<String>();
         ArrayList<Object> Student = new ArrayList<Object>();
+        ArrayList<Integer> marks = new ArrayList<Integer>();
+        ArrayList<Object> books = new ArrayList<Object>();
+        Book b = new Book("shopaholic",99.8);
+       // Book1 b1 = new Book1("lean start up",93.7);
+        books.add(new Book("Book 87", 250.0));
+        books.add(new Book("Book 88", 150.0));
+        books.add(new Book("Book 89", 300.0));
+        books.add(b);
+        //books.add(b1);
         myNumbers.add(10);
         myNumbers.add(15);
         myNumbers.add(20);
@@ -35,7 +101,17 @@ public class ArrayPractice {
 
         Student s = new Student();
         Student.add(s);
+        marks.add(98);
+        marks.add(99);
+        marks.add(91);
+        marks.add(93);
+        double marksAverage =calculateAverage(marks); //calculates average marks of a student
 
+//        for(Object book : books){
+//            if(book.getPrice()<99){
+//                System.out.println("The book less than 99 is "+book);
+//            }
+//        }
         System.out.println("ArrayList class in Lists package ");
         System.out.println("My numbers array "+myNumbers);
         System.out.println("MAX number in myNumbers array is "+max);
@@ -43,5 +119,7 @@ public class ArrayPractice {
         System.out.println("The updated string is "+str);
         System.out.println("The sorted string is "+ str);
         System.out.println(Student +"This is a student object updated  ");
+        System.out.println("Average marks of a student : "+marksAverage);
+        System.out.println(books+"This is a Book list object  ");
     }
 }
