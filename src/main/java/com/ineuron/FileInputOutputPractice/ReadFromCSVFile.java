5f085bfbcb1fa689b9a322f334662b6406c52ddb
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-05-04 09:30:18 -0500

    Revised java practice day 16 File I/O operations Questions 164  =>patially completed

--- src/main/java/com/ineuron/FileInputOutputPractice/ReadFromCSVFile.java
+++ src/main/java/com/ineuron/FileInputOutputPractice/ReadFromCSVFile.java
@@ -1,7 +1,29 @@
 package com.ineuron.FileInputOutputPractice;
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.util.List;
 
 public class ReadFromCSVFile {
+
     public static void main(String[] args){
 
+        String fileName = "customers.csv";
+        //try with resources
+        try(CSVReader reader = new CSVReader(new FileReader(fileName))){
+            List <String[]> records = reader.readAll();
+            //print header
+            String[] header = records.get(0);
+            printRow(header);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    private static void printRow(String[] row) {
+        for (String cell : row) {
+            System.out.print(cell + "\t");
+        }
+        System.out.println();
     }
 }
