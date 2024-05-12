70042c2b4aec77c4868f39eabef5655b189d676a
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-05-12 07:26:06 -0500

    Deleting and Inserting records in DB

---
+++ src/main/java/com/ineuron/AdvanceJava/Delete.java
@@ -0,0 +1,34 @@
+package com.ineuron.AdvanceJava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Delete {
+    public static void main(String[] args){
+        // 1) Load and register the driver -> Automated from JDBC 4.X
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // 2) Establish the connection
+            String userName = "root";
+            String password = "123456";
+            String URL = "jdbc:mysql:///demodb";
+            Connection connection = DriverManager.getConnection(URL, userName, password);
+            // 3) create statement object and send the query
+            Statement statement = connection.createStatement();
+            // 4) Execute the query and process the result set
+            String sqlDeleteQuery = "delete from MyTable where id =1";
+            int rowsAffected = statement.executeUpdate(sqlDeleteQuery);
+            System.out.println("No of rows affected is : "+ rowsAffected);
+            //ResultSet resultSet = statement.executeQuery(sqlDeleteQuery);
+            // 5) handle all the exceptions -> catch block
+            // 6) close all the resources
+           // resultSet.close();
+            statement.close();
+            connection.close();
+        }catch (Exception ce){
+            ce.printStackTrace();
+        }
+    }
+}
